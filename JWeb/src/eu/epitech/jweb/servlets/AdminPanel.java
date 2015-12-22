02156c9fdda4b15333c10e04b98664e31467a326
jordan chazottes <jordan.chazottes@epitech.eu>
2015-12-22 17:01:44 +0100

    admin

---
+++ JWeb/src/eu/epitech/jweb/servlets/AdminPanel.java
@@ -0,0 +1,57 @@
+package eu.epitech.jweb.servlets;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import eu.epitech.jweb.admin.Administration;
+import eu.epitech.jweb.beans.User;
+
+public class AdminPanel extends HttpServlet {
+	public static final String VIEW = "/WEB-INF/adminPanel.jsp";
+	public static final String ATT_USERS = "users";
+	public static final String ATT_SESSION_USER = "userSession";
+	public static final String PUBLIC_ACCESS = "/profilPublic.jsp";
+	public static final String PRIVATE_ACCESS = "/admin/adminPanel.jsp";
+	public static List<User> userList = null;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String redirect = "";
+		
+		if (session.getAttribute(ATT_SESSION_USER) == null)
+			redirect = request.getContextPath() + PUBLIC_ACCESS;
+		else {
+			if (((User) session.getAttribute(ATT_SESSION_USER)).getAdmin().equals("0")) {
+				redirect = request.getContextPath() + PUBLIC_ACCESS;
+			} else {
+				Administration admin = new Administration();
+				userList = admin.getUserslist(request);
+				request.setAttribute(ATT_USERS, userList);
+				redirect = request.getContextPath() + PRIVATE_ACCESS;
+			}
+		}
+		if (redirect.equals(request.getContextPath() + PUBLIC_ACCESS))
+			response.sendRedirect(redirect);		
+		else
+			this.getServletContext().getRequestDispatcher(PRIVATE_ACCESS).forward(request, response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		Administration admin = new Administration();
+		for (User user : userList)
+		{
+			if ((request.getParameter("set" + user.getEmail())) != null)
+				System.out.println("button clicked : set" + user.getEmail());
+			if ((request.getParameter("unset" + user.getEmail())) != null)
+				System.out.println("button clicked : unset" + user.getEmail());
+		}
+        this.getServletContext().getRequestDispatcher(PRIVATE_ACCESS).forward(request, response);
+	}
+}
